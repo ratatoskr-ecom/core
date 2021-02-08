8f7d0ba20f56a636a05a853e0415a951c5b42113
Ronan 'Zero' Schwarz <zero@sqrls.de>
2021-02-08 20:01:59 +0100

    a marker for userdata

---
+++ lib/src/main/java/de/sqrls/ratatoskr/core/user/IUserData.java
@@ -0,0 +1,11 @@
+package de.sqrls.ratatoskr.core.user;
+
+import de.sqrls.ratatoskr.core.IBaseModel;
+
+/**
+ * Marker interface for User Data.
+ * This can be Data about a User (profile,..)
+ * or Data that a User has created (Notes, Preferences,..)
+ */
+public interface IUserData extends IBaseModel {
+}
